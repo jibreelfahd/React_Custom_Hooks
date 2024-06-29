535db7fe7c984a58c0366e345fd68b0b4e82db87
Fahd Jibril <jibreelfahd@gmail.com>
2024-06-29 22:40:13 +0100

    Initial commit, using Custom Hooks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ForwardCounter from './components/ForwardCounter';
+import BackwardCounter from './components/BackwardCounter';
+
+const App = () => {
+  return (
+    <>
+     <ForwardCounter />
+     <BackwardCounter />
+    </>
+  );
+};
+
+export default App;
